77fbd6b8b06492587c55897923eeb8f891d3d5d3
Lawrence Taylor <lawrence.taylor@outlook.com>
2018-12-05 10:01:04 +0000

    Day 5

---
+++ src/Day05.hs
@@ -0,0 +1,36 @@
+module Day05 where
+
+import Common
+import Data.Char
+
+reacts :: Char -> Char -> Bool
+reacts c1 c2 = (==)32 $ abs $ ord c1 - ord c2
+
+innerReact :: Char -> String -> String
+innerReact c (x:xs) | reacts c x = xs
+innerReact c x =  c : x
+
+react :: String -> String
+react = foldr innerReact []
+
+rawData :: IO String
+rawData = readData "data\\Day05"
+
+partOne :: String -> Int
+partOne =  length .  react
+
+removeUnits :: String -> (Char, Char) -> String
+removeUnits s (c1, c2) = filter(\x -> x /= c1 && x /= c2) s
+
+testStrings :: String -> [String]
+testStrings input = 
+  removeUnits input <$> zip ['a'..'z'] ['A'..'Z']
+
+partTwo :: String -> Int
+partTwo input = minimum $ partOne <$> testStrings input
+
+main :: IO ()
+main = do
+  input <- rawData
+  putStrLn $ show $ partOne input -- "dabAcCaCBAcCcaDA"
+  putStrLn $ show $ partTwo input --  "dabAcCaCBAcCcaDA"
